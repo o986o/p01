e5604bd8e6bbb2bcfd337aa68f473105290288bd
doheejeon <jeondohee1@gmail.com>
2017-11-21 22:43:26 +0100

    01

--- sketch.js
+++ sketch.js
@@ -1,26 +1,40 @@
-var pg;
+var posX, posy, direction;
+var stepSize = 5;
+var stepX = 80;
+var stepY = 30;
 
-function setup(){
-  createCanvas(1440, 80);
-  background(100,0);
-  pg = createGraphics(200, 130);
+// human readable direction
+//번호는 순서의 의미를 가지고 있기 때문에 마음대로 바꿀 수 없다.
+var NORTH = 0;
+var EAST = 1;
+var SOUTH = 2;
+var WEST = 3;
+
+function setup()
+{
+  createCanvas(1440, 720);
+
+  background(255,255,0);
+
+  posx = width/2;
+  posy = height/2;
+  stroke(255,0,0,100);
 }
 
+
 function draw(){
-  fill(255, 30);
-  rect(0, 0, width, height);
-  fill(0, 0, 255);
-  noStroke();
-  ellipse(mouseX, mouseY, 50, 50);
 
-  pg.background(0.1);
-  pg.noFill(0);
-  pg.stroke(255);
-  var pgx = map(mouseX, 0, width, 0,200) + 1200;
-  var pgy = map(mouseY, 0, width, 0,130) + 550;
-  console.log(pgx);
-  pg.ellipse(pgx, pgy, 5, 5);
+  stepX=max(mouseX,100); //두번째 방식
+  stepY=max(mouseY,10);
 
-  //Draw the offscreen buffer to the screen with image()
-  image(pg, 1200, 550);
+  for(var y=0; y<height; y=y+stepY){ //아래로 향하는 바깥쪽 반복문 (수직)
+
+      for(var x=0; x<width; x+=stepX){ //오른쪽으로 향하는 반복문 (수평)
+
+        stroke(0,0,255);
+        fill(x,height-y,1);
+        //rect(도형의 위치x, 도형의 위치y, 도형의 크기x, 도형의 크기y)
+        rect(x,y,stepX,stepY);
+      }
+  }
 }
